0f9d3b1eb169aded63f8b9fda15060f9a3673139
guptadeepak8 <vectord7738@gmail.com>
2026-08-01 22:18:12 +0530

    fix:updated token

--- apps/server/src/modules/auth/auth.controller.ts
+++ apps/server/src/modules/auth/auth.controller.ts
@@ -47,7 +47,7 @@ export async function loginController(
 res.cookie("accessToken", token, {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "lax",
+  sameSite: "none",
   maxAge: 24 * 60 * 60 * 1000,
 });
 
